8ceace3f37e521437e4b25d1942cbaa2604746d0
Lavi <lavisharma796@gmail.com>
2019-01-10 19:47:30 +0000

    Updates

---
+++ precache-manifest.0dadca668664db35a131791ac580d17d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32c8586b1c359440667a",
+    "url": "/Youtube-Search-Play/static/js/runtime~main.32c8586b.js"
+  },
+  {
+    "revision": "f7d5c3e9824b5647cb49",
+    "url": "/Youtube-Search-Play/static/js/main.f7d5c3e9.chunk.js"
+  },
+  {
+    "revision": "99d9b32e70d8f9dc890d",
+    "url": "/Youtube-Search-Play/static/js/1.99d9b32e.chunk.js"
+  },
+  {
+    "revision": "f7d5c3e9824b5647cb49",
+    "url": "/Youtube-Search-Play/static/css/main.fd6558c0.chunk.css"
+  },
+  {
+    "revision": "326fdb33adbdab3654e9f07de6fe7261",
+    "url": "/Youtube-Search-Play/index.html"
+  }
+];
