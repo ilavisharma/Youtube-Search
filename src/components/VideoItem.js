83ba48bac30d7fd658c6431ef9c572d685fd4a10
Lavi <lavisharma796@gmail.com>
2019-01-10 19:41:43 +0000

    fix previos issues, added video player

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -2,9 +2,8 @@ import React from 'react';
 import './VideoItem.css';
 
 const VideoItem = ({ video, onVideoSelect }) => {
-    console.log(video);
     return (
-        <div onClick={video => onVideoSelect(video)} className="item video-item">
+        <div onClick={ () => onVideoSelect(video)} className="item video-item">
             <img alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url} />
             <div className="content">
                 <div className="header">{video.snippet.title}</div>
